5e21dc20866073684245798f8757e844bb846cc2
swampdave <35638215+swampdave@users.noreply.github.com>
2026-07-18 23:28:15 -0400

    Adding new files from Mini-IT13

---
+++ mytest.py
@@ -0,0 +1,10 @@
+name = "Carol"
+age = 3000
+if name == "Alice":
+    print("Hello, Alice!")
+elif age < 12:
+    print("You are not Alice, kiddo.")
+elif age > 100:
+    print("You are not Alice, grannie.")
+elif age > 2000:
+    print("Unlike you Alice is not a undead, immortal vampire.")
